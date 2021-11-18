b71b6b0591b2a08e0c0d477a353fb657d2140735
slw515 <slw515@nyu.edu>
2021-11-18 13:20:31 -0500

    change background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="app"
+      style={{ backgroundImage: "url(assets/background.png)" }}
+    >
+      <img src={"assets/logo.png"} id="logo-image" />
+      <Card></Card>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <img class="topCardImage" src={"assets/interview.png"} />
+      <p>Question 1</p>
+      <h1>What is the symbol for the EOS Blockchain</h1>
+      <div className="button-wrapper">
+        <button>Chestahedron</button>
+        <button>Tetrahedron</button>
+        <button>Dodecahedron</button>
+        <button>Polyhedron</button>
+      </div>
+    </div>
+  );
+}
