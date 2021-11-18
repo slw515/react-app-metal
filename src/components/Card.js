eb586364a1799c636254dc8f52b9e250d46e9753
slw515 <slw515@nyu.edu>
2021-11-18 13:25:07 -0500

    final changes to copy

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default function Card() {
     <div className="card">
       <img class="topCardImage" src={"assets/interview.png"} />
       <p>Question 1</p>
-      <h1>What is the symbol for the EOS Blockchain</h1>
+      <h1>What is the symbol for the EOS Blockchain?</h1>
       <div className="button-wrapper">
         <button>Chestahedron</button>
         <button>Tetrahedron</button>
